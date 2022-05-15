5cd1cabf3477585c5ba87399ebfa50b892689900
a32fred <a.32.freeed@gmail.com>
2022-05-15 12:58:55 -0300

    começo da aplicação

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch } from 'react-icons/fi';
+import './style.css';
 
-function App() {
+const App = ()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Onde é esse CEP?</h1>
+
+      <div className="inputContainer">
+        <input type="text" placeholder="Digite aqui o cep"/>
+
+        <button className="buttonSearch">
+          <FiSearch size={ 25 } color="#fff"/>
+        </button>
+
+      </div>
+
+      <main className="main">
+        <h2>CEP: 61890000</h2>  
+
+        <span>Rua: Sinval Leitão</span>
+        <span>Complemento: Algum aí</span>
+        <span>Bairro: 17 de março</span>
+        <span>Localidade: Ceara - CE</span>
+
+      </main>
+
     </div>
   );
 }
